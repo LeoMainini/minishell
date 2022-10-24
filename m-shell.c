701e327125f718bbfd08cbc52004aed375dacb8f
Bernardo Carreira monico <bcarreir@c2r3s2.42lisboa.com>
2022-10-24 16:20:43 +0100

    fixed the wrong folder

--- m-shell.c
+++ m-shell.c
@@ -3,21 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   m-shell.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: leferrei <leferrei@student.42lisboa.c      +#+  +:+       +#+        */
+/*   By: bcarreir <bcarreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 16:23:29 by leferrei          #+#    #+#             */
-/*   Updated: 2022/10/21 16:23:31 by leferrei         ###   ########.fr       */
+/*   Updated: 2022/10/24 16:06:35 by bcarreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+#include "minishell.h"
 
 void exit_status(int status, char **line)
 {
-
 	free(*line);
 	rl_clear_history();
 	exit(status);
@@ -27,12 +23,14 @@ int	main(void)
 {
 	char *read_line;
 
+	signal(SIGINT, sighandler);
+	signal(SIGQUIT, sighandler);
 	read_line = readline("shell:>");
 	while (read_line)
 	{
 		add_history(read_line);
-
 		free(read_line);
+		read_line = NULL;
 		read_line = readline("shell:>");
 	}
 	exit_status(1, &read_line);
