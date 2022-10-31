f448d040ec9f636bd51ee49a652d44b08bbb88fd
Leonardo osvaldo Ferreira mainini <leferrei@student.42lisboa.com>
2022-10-31 17:51:12 +0000

    update interpreter to find variables

--- interpreter.c
+++ interpreter.c
@@ -6,7 +6,7 @@
 /*   By: leferrei <leferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 16:52:29 by leferrei          #+#    #+#             */
-/*   Updated: 2022/10/31 17:36:29 by leferrei         ###   ########.fr       */
+/*   Updated: 2022/10/31 17:49:34 by leferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,10 +37,13 @@ char	*remove_char(char *str, int index)
 
 void	interpret_strings(t_cmdd *argd, t_ms *data)
 {
-	int	i;
-	int	k = -1;
-	int	in_doubles = 0;
-	int	in_singles = 0;
+	int		i;
+	int		k = -1;
+	int		j;
+	int		in_doubles = 0;
+	int		in_singles = 0;
+	char	*temp;
+	char	*substr;
 
 	(void)data;
 	while (argd->args[++k])
@@ -50,7 +53,6 @@ void	interpret_strings(t_cmdd *argd, t_ms *data)
 		{
 			if (argd->args[k][i] == '"' && !in_doubles && !in_singles)
 			{
-				printf("removing character %c from index %d\n", argd->args[k][i], i);
 				argd->args[k] = remove_char(argd->args[k], i);
 				in_doubles = 1;
 			}
@@ -61,7 +63,6 @@ void	interpret_strings(t_cmdd *argd, t_ms *data)
 			}
 			if (argd->args[k][i] == 39 && !in_singles && !in_doubles)
 			{
-				printf("removing character %c from index %d\n", argd->args[k][i], i);
 				argd->args[k] = remove_char(argd->args[k], i);
 				in_singles = 1;
 			}
@@ -69,6 +70,18 @@ void	interpret_strings(t_cmdd *argd, t_ms *data)
 			{
 				argd->args[k] = remove_char(argd->args[k], i);
 				in_singles = 0;
+			}
+			if (in_doubles && argd->args[k][i] == '$')
+			{
+				j = 0;
+				while (!ft_isspace(argd->args[k][(i + 1) + j]) && argd->args[k][(i + 1) + j] != '"')
+					j++;
+				substr = ft_substr(argd->args[k], i + 1, j);
+				printf("var = %s\n", substr);
+				temp = *get_env(substr, data);
+				if (temp)
+					temp = &temp[ft_strlen(substr) + 1];
+				printf("val = %s\n", temp);
 			}
 
 		}
