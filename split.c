88cc73a7c9e1be9c751a4ef74c958fcd7cbed999
Bernardo <bcmonico@gmail.com>
2022-11-08 16:56:37 +0000

    fixed cmd split. working for most cases

--- split.c
+++ split.c
@@ -6,7 +6,7 @@
 /*   By: ben <ben@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 18:45:17 by bcarreir          #+#    #+#             */
-/*   Updated: 2022/11/08 13:34:36 by ben              ###   ########.fr       */
+/*   Updated: 2022/11/08 16:56:03 by ben              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,7 @@ int	ffquotedtext(t_spl *spl, char *s, int *j, char qt)
 int	isvalidcmd(char *s,  t_spl *spl)
 {
 	int	j;
+	int	k;
 
 	j = 0;
 	if (*s)
@@ -74,7 +75,10 @@ int	isvalidcmd(char *s,  t_spl *spl)
 		}
 		if (s[j] == '|')
 		{
-			if (s[j+1] == '|' || j == 0)
+			k = j + 1;
+			while(s[k] && ft_isspace(s[k]))
+				k++;
+			if (s[k]== '|' || s[j+1] == '|' || j == 0)
 			{
 				printf("parse error near `|'\n");
 				return (1);
@@ -129,7 +133,7 @@ static int	ft_alloc_arg(char **b,char *str)
 		}
 		i++;
 	}
-	*b = malloc(sizeof(char) * (i + 1));
+	*b = malloc(sizeof(char) * (50 + 1));
 	if (!b)
 		return (1);
 	printf("%d arg size\n", i);
@@ -145,7 +149,7 @@ static int	ft_alloc_cmd(char ***b, char *str)
 	j = 0;
 	while (str[i])
 	{
-		while (ft_isspace(str[i]))
+		while (str[i] && ft_isspace(str[i]))
 			i++;
 		if (str[i] && str[i] == '|' && isvalidpipe(str, i))
 		{
@@ -161,14 +165,168 @@ static int	ft_alloc_cmd(char ***b, char *str)
 			j++;
 		}
 	}
-	*b = malloc(sizeof(char*) * (j + 1));
+	*b = malloc(sizeof(char*) * (50 + 1));
 	if (!b)
 		return (1);
 	printf("%d cmd size\n", j);
 	return (0);
 }
 
+int	ft_argspercmd(t_spl *spl, char *s)
+{
+	static int	i;
+	int			argc;
+
+	argc = 0;
+	while (s[i])
+	{
+		while (s[i] && ft_isspace(s[i]))
+			i++;
+		if (s[i] && s[i] != '|')
+		{
+			argc++;
+			if (s[i] == 34 || s[i] == 39)
+			{
+				ffquotedtext(spl, s, &i, s[i]);
+				continue ;
+			}
+			while (s[i] && s[i] != '|' && !ft_isspace(s[i]))
+				i++;
+
+		}
+		if (s[i] && s[i] == '|')
+		{
+			i++;
+			break ;
+		}
+	}
+	// printf("args per cmd %d\n", argc);
+	return (argc);
+}
+
+int	ft_argsize(char *s, int i)
+{
+	char	q;
+	int		j;
+
+	j = i;
+	while (s[i])
+	{
+		if (s[i] && (s[i] == 34 || s[i] == 39))
+		{
+			q = s[i];
+			i++;
+			while (s[i] && s[i] != q)
+				i++;
+			if (s[i] && s[i] == q)
+			{
+				i++;
+				continue ;
+			}
+		}
+		else if (s[i] && !ft_isspace(s[i]) && s[i] != '|')
+			i++;
+		else if (s[i] && (ft_isspace(s[i]) || s[i] == '|'))
+			break ;
+	}
+	// printf("argsize %d\n", i - j);
+	return (i - j);
+}
+
 char	***cmd_split(char *s)
+{
+	t_spl	spl;
+	char	q;
+	int		i;
+	int		j;
+	int		l;
+	int		k;
+
+	if (!s)
+		return (NULL);
+	init_spl(&spl);
+	if (isvalidcmd(s, &spl))
+		return (NULL);
+	spl.ss = (char ***)malloc(sizeof(char **) * (spl.cmd_count + 1));
+	if (!spl.ss)
+		return (NULL);
+	spl.ss[spl.cmd_count] = NULL;
+	l = 0;
+	while (l < spl.cmd_count)
+	{
+		spl.ss[l] = malloc(sizeof(char*) * (ft_argspercmd(&spl, s) + 1));
+		if (!spl.ss[l])
+			return (NULL);
+		l++;
+	}
+	i= 0;
+	j = 0;
+	l = 0;
+	while (s[i])
+	{
+		while (s[i] && ft_isspace(s[i]))
+				i++;
+		if (s[i] && s[i] == '|')
+		{
+			// printf("NEW PIPE l%d j%d\n", l, j);
+			spl.ss[l][j] = NULL;
+			l++;
+			j = 0;
+			i++;
+			while (s[i] && ft_isspace(s[i]))
+				i++;
+		}
+		if (s[i] && s[i] != '|')
+		{
+			spl.ss[l][j] = malloc(sizeof(char) * (ft_argsize(s, i) + 1));
+			if (!spl.ss[l][j])
+				return (NULL);
+		}
+		// printf("l%d j%d\n", l, j);
+		k = 0;
+		while (s[i])
+		{
+			while (s[i] && !ft_isspace(s[i]) && s[i] != '|')
+			{
+				if (s[i] && (s[i] == 34 || s[i] == 39))
+				{
+					q = s[i];
+					spl.ss[l][j][k++] = s[i++];
+					while (s[i] && s[i] != q)
+						spl.ss[l][j][k++] = s[i++];
+					if (s[i] && s[i] == q)
+					{
+						spl.ss[l][j][k++] = s[i++];
+						continue ;
+					}
+				}
+				spl.ss[l][j][k++] = s[i++];
+			}
+		// printf("K is %d\n", k);
+			if (s[i] && (ft_isspace(s[i]) || s[i] == '|'))
+				break ;
+		}
+		spl.ss[l][j][k] = '\0';
+		j++;
+	}
+	spl.ss[l][j] = NULL;
+	spl.ss[++l] = NULL;
+	l = 0;
+	j = 0;
+	while (spl.ss[l])
+	{
+		j = 0;
+		while (spl.ss[l][j])
+		{
+			printf("%d %d %s\n",l,j, spl.ss[l][j]);
+			j++;
+		}
+		l++;
+	}
+	return (spl.ss);
+}
+
+char	***cmd_split2(char *s)
 {
 	t_spl	spl;
 	int		i;
@@ -184,37 +342,33 @@ char	***cmd_split(char *s)
 	spl.ss = (char ***)malloc(sizeof(char **) * (spl.cmd_count + 1));
 	if (!spl.ss)
 		return (NULL);
+	if (ft_alloc_cmd(&*spl.ss, s))
+		return (NULL);
 	printf("%d cmds\n", spl.cmd_count);
 	l = 0;
 	j = 0;
 	i = 0;
 	while (l != spl.cmd_count)
 	{
-		if (j == 0)
-		{	
-			ft_alloc_cmd(&spl.ss[l], s);
-			l++;
-		}
-		else if (s[i] && s[i] == '|' && j > 0)
+		while (s[i] && ft_isspace(s[i]))
+				i++;
+		if (s[i] && s[i] == '|')
 		{
 			i++;
 			l++;
-			j=0;
 			ft_alloc_cmd(&spl.ss[l], s);
+			j = 0;
+			if (ft_alloc_arg(&spl.ss[l][j], s))
+					return (NULL);
 		}
-		else
+		else if (s[i] && s[i] != '|' && j)
 		{
-			while (s[i] && ft_isspace(s[i]))
-				i++;
 			if (s[i] && s[i] != '|')
 				if (ft_alloc_arg(&spl.ss[l][j], s))
-				{
-					j++;
 					return (NULL);
-				}
 		}
 		k = 0;
-		while (s[i])
+		while (s[i] && !ft_isspace(s[i]))
 		{
 			if (s[i] == '|' && isvalidpipe(s, i))
 				break ;
@@ -223,10 +377,11 @@ char	***cmd_split(char *s)
 			i++;
 		}
 		spl.ss[l][j][k] = '\0';
+		j++;
 	}
 	spl.ss[l][j] = NULL;
-	write(1, "end\n", 4);
-	if (*spl.ss)
+	spl.ss[l + 1] = NULL;
+	if (spl.ss[l])
 	{	
 		if (spl.ss[l] && checkemptycmds(spl.ss[l]))
 		{
@@ -235,7 +390,15 @@ char	***cmd_split(char *s)
 		}
 	}
 	j = 0;
-	while (spl.ss[l][j])
-		printf("%s\n", spl.ss[l][j++]);
+	while (spl.ss[l] && spl.ss[l][j])
+	{
+		printf("%d %d %s\n",l,j, spl.ss[l][j]);
+		j++;
+		if (!spl.ss[l][j])
+		{
+			j = 0;
+			l++;
+		}
+	}
 	return (spl.ss);
 }
