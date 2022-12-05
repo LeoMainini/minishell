a5352a89822eb6f679a6e1682f7a4c61315c2e34
Leonardo osvaldo Ferreira mainini <leferrei@student.42lisboa.com>
2022-12-05 13:41:56 +0000

    format export and unset

--- builtins/export.c
+++ builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: leferrei <leferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 13:20:54 by leferrei          #+#    #+#             */
-/*   Updated: 2022/12/05 13:30:25 by leferrei         ###   ########.fr       */
+/*   Updated: 2022/12/05 13:41:26 by leferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,13 @@ char	*get_var_name(t_cmdd *argd, int i)
 {
 	if (ft_strchr(argd->args[i], '=') && scmp(argd->args[i], "="))
 		return (ft_substr(argd->args[i], 0,
-			(ft_strchr(argd->args[i], '=') - argd->args[i])));
-	else 
+				(ft_strchr(argd->args[i], '=') - argd->args[i])));
+	else
 		return (ft_strdup(argd->args[i]));
 }
 
 int	name_invalid(char *name)
 {
-	int	result;
-
 	if (!is_alphastr(name))
 	{
 		ft_putstr_fd("export: '", STDERR_FILENO);
@@ -37,10 +35,35 @@ int	name_invalid(char *name)
 	return (0);
 }
 
+void	add_genv_value(char **envi, t_cmdd *argd, int i, char *name)
+{
+	int	last_i;
+
+	if (envi)
+	{
+		if (ft_strchr(argd->args[i], '='))
+		{
+			free(*envi);
+			*envi = ft_strdup(argd->args[i]);
+		}
+		free(name);
+	}
+	else
+	{
+		free(name);
+		envi = g_envs;
+		g_envs = duplicate_envp(g_envs, 1, 1);
+		free(envi);
+		last_i = 0;
+		while (g_envs[last_i])
+			last_i++;
+		g_envs[last_i] = ft_strdup(argd->args[i]);
+	}
+}
+
 int	export(t_cmdd *argd, t_ms *data, int before_pipe)
 {
 	char	**envi;
-	int		last_i;
 	char	*name;
 	int		i;
 	int		result;
@@ -53,29 +76,15 @@ int	export(t_cmdd *argd, t_ms *data, int before_pipe)
 	while (argd->args[++i])
 	{
 		name = get_var_name(argd, i);
+		if (!result)
+			result = name_invalid(name);
 		if (name_invalid(name) || before_pipe)
+		{
+			free(name);
 			continue ;
+		}
 		envi = get_env(name, data);
-		if (envi)
-		{
-			if (ft_strchr(argd->args[i], '='))
-			{
-				free(*envi);
-				*envi = ft_strdup(argd->args[i]);
-			}
-			free(name);
-		}
-		else
-		{
-			free(name);
-			envi = g_envs;
-			g_envs = duplicate_envp(g_envs, 1, 1);
-			free(envi);
-			last_i = 0;
-			while (g_envs[last_i])
-				last_i++;
-			g_envs[last_i] = ft_strdup(argd->args[i]);
-		}
+		add_genv_value(envi, argd, i, name);
 	}
 	return (set_ret_return(data, result));
 }
